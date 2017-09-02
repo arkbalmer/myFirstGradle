e71540c0d51f47d22a4d73e5144d40cdc60316d4
alex <holod87.87@gmail.com>
2017-09-02 16:15:41 +0300

    created class Book

---
+++ src/main/java/com/github/arcbalmer/mfg/Book.java
@@ -0,0 +1,35 @@
+package com.github.arcbalmer.mfg;
+
+import java.util.Date;
+
+public class Book {
+
+    private String name;
+    private Date publishingDate;
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getPublishingDate() {
+        return publishingDate;
+    }
+
+    public void setPublishingDate(Date publishingDate) {
+        this.publishingDate = publishingDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
