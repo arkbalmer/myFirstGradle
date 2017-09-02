4e35d7a823ddda454230178e3ffd0738f1c6a6cb
alex <holod87.87@gmail.com>
2017-09-02 17:18:06 +0300

    BookController serves rest requests

--- src/main/java/com/github/arcbalmer/mfg/rest/BookController.java
+++ src/main/java/com/github/arcbalmer/mfg/rest/BookController.java
@@ -2,9 +2,12 @@ package com.github.arcbalmer.mfg.rest;
 
 import com.github.arcbalmer.mfg.Book;
 import com.github.arcbalmer.mfg.service.BookService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
 public class BookController {
 
     private BookService bookService;
@@ -13,6 +16,7 @@ public class BookController {
         bookService = new BookService();
     }
 
+    @RequestMapping("/books")
     public List<Book> getAllBooks() {
         return bookService.getBooks();
     }
